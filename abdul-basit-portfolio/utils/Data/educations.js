988897cf5ebf80cb771b08502b2c476f10b05fcc
Ghost-oo5 <rajaabdulbasit005@gmail.com>
2024-12-29 13:03:18 +0500

    💥 NEW: Added Education Section

---
+++ abdul-basit-portfolio/utils/Data/educations.js
@@ -0,0 +1,20 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor Degree",
+    duration: "2020 - Present",
+    institution: "University of Chakwal",
+  },
+  {
+    id: 2,
+    title: "College Certificate",
+    duration: "2018 - 2020",
+    institution: "Royal College of Scienes Chakwal",
+  },
+  {
+    id: 3,
+    title: "Higher Secondary Certificate",
+    duration: "2016 - 2018",
+    institution: "Govt High  Schoo No.1 Chakwal",
+  }
+]
