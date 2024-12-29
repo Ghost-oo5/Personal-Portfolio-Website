48b480665d32651425bee5cff77fd97bcaef69c3
Ghost-oo5 <rajaabdulbasit005@gmail.com>
2024-12-29 11:41:18 +0500

    💥 NEW: Added Experience Section and also made imporvements in existing code

--- abdul-basit-portfolio/src/app/page.tsx
+++ abdul-basit-portfolio/src/app/page.tsx
@@ -1,9 +1,24 @@
+import About from "./components/pages/about/page";
+import Blogs from "./components/pages/Blogs/page";
+import Contact from "./components/pages/Contact/page";
+import Education from "./components/pages/education/page";
+import Experience from "./components/pages/experience/page";
 import HeroSection from "./components/pages/hero-section/page";
+import Projects from "./components/pages/Projects/page";
+import Skills from "./components/pages/Skills/page";
+
 import './css/card.css';
 export default function Home() {
   return (
     <>
       <HeroSection/>
+      <About/>
+      <Experience/>
+      <Skills/>
+      <Projects/>
+      <Education/>
+      <Blogs/>
+      <Contact/>
     </>
   );
 }
