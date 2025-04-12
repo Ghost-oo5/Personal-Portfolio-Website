4cc600684c8d2d7c93d25da01491c22301f67cb0
Ghost-oo5 <rajaabdulbasit005@gmail.com>
2025-04-12 18:37:04 +0500

    💄 UI:   Added section heading to projects section

--- src/app/components/pages/Projects/index.tsx
+++ src/app/components/pages/Projects/index.tsx
@@ -5,21 +5,32 @@ const Projects = () => {
   return (
     <section
       id="projects"
-      className="container mx-auto py-20 px-4 sm:px-8 bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-800 dark:to-gray-900"
     >
-      <div className="mb-12 text-center">
-        <h2 className="text-4xl font-bold text-gray-900 dark:text-gray-100">
-          Projects Showcase
-        </h2>
-        <p className="mt-4 max-w-2xl mx-auto text-gray-600 dark:text-gray-300">
-          A selection of creative and innovative projects, each crafted with
-          passion and precision.
-        </p>
+      <div className="sticky top-10">
+        <div className="w-[100px] h-[90px]  rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl  opacity-30"></div>
+        <div className="flex items-center justify-start relative">
+          <span className="bg-[#1a1443] absolute left-0  w-fit text-white px-5 py-3 text-xl rounded-md">
+            PROJECTS
+          </span>
+          <span className="w-full h-[2px] bg-[#1a1443]"></span>
+        </div>
       </div>
-      <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
-        {projectsData.map((project) => (
-          <ProjectCard key={project.id} project={project} />
-        ))}
+      <div className="container mt-24 mx-auto py-20 px-4 sm:px-8   from-purple-800 to-purple-900"
+      >
+        <div className="mb-12 text-center">
+          <h2 className="text-4xl font-bold text-gray-900 dark:text-gray-100">
+            Projects Showcase
+          </h2>
+          <p className="mt-4 max-w-2xl mx-auto text-gray-600 dark:text-gray-300">
+            A selection of creative and innovative projects, each crafted with
+            passion and precision.
+          </p>
+        </div>
+        <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
+          {projectsData.map((project) => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
+        </div>
       </div>
     </section>
   );
