9550b2e2a9714cf5f6eecdefabe4bee030b07ff4
Ghost-oo5 <rajaabdulbasit005@gmail.com>
2025-06-26 12:53:48 +0500

    💥 NEW:  Added tilt to experience cards

--- src/app/components/experience/page.tsx
+++ src/app/components/experience/page.tsx
@@ -5,6 +5,7 @@ import { BsPersonWorkspace } from "react-icons/bs";
 import AnimationLottie from "@/app/components/helper/animation-lottie";
 import GlowCard from "@/app/components/helper/glow-card";
 import experience from "@/../public/lottie/code.json";
+import Tilt from "react-parallax-tilt";
 
 function Experience() {
   return (
@@ -41,38 +42,40 @@ function Experience() {
           <div>
             <div className="flex flex-col gap-6">
               {experiences.map((experience) => (
-                <GlowCard
-                  key={experience.id}
-                  identifier={`experience-${experience.id}`}
-                >
-                  <div className="p-3 relative">
-                    <Image
-                      src="./svg/common/blur-23.svg"
-                      alt="Hero"
-                      width={1080}
-                      height={200}
-                      className="absolute bottom-0 opacity-80"
-                    />
-                    <div className="flex justify-center">
-                      <p className="text-xs sm:text-base text-[#16f2b3]">
-                        {experience.duration}
-                      </p>
-                    </div>
-                    <div className="flex items-center gap-x-8 px-3 py-5">
-                      <div className="text-violet-500  transition-all duration-300 hover:scale-125">
-                        <BsPersonWorkspace size={36} />
-                      </div>
-                      <div>
-                        <p className="text-base sm:text-xl mb-2 font-medium uppercase">
-                          {experience.title}
-                        </p>
-                        <p className="text-base sm:text-base lg:text-lg">
-                          {experience.company}
+                <Tilt>
+                  <GlowCard
+                    key={experience.id}
+                    identifier={`experience-${experience.id}`}
+                  >
+                    <div className="p-3 relative">
+                      <Image
+                        src="./svg/common/blur-23.svg"
+                        alt="Hero"
+                        width={1080}
+                        height={200}
+                        className="absolute bottom-0 opacity-80"
+                      />
+                      <div className="flex justify-center">
+                        <p className="text-xs sm:text-base text-[#16f2b3]">
+                          {experience.duration}
                         </p>
                       </div>
+                      <div className="flex items-center gap-x-8 px-3 py-5">
+                        <div className="text-violet-500  transition-all duration-300 hover:scale-125">
+                          <BsPersonWorkspace size={36} />
+                        </div>
+                        <div>
+                          <p className="text-base sm:text-xl mb-2 font-medium uppercase">
+                            {experience.title}
+                          </p>
+                          <p className="text-base sm:text-base lg:text-lg">
+                            {experience.company}
+                          </p>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </GlowCard>
+                  </GlowCard>
+                </Tilt>
               ))}
             </div>
           </div>
